2f2d22f9afc7123ddc73af67846ca0d7599d8011
mkojoa <michaelameyaw7@gmail.com>
2022-03-13 16:23:21 +0000

    Add project files.

---
+++ Hubtel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Hubtel.QuickHelpers;
+
+namespace Hubtel
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+           var result =  await HubtelExtensions.PrepareSmsApiCall();
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+ 
+
---
+++ Hubtel/QuickHelpers/HubtelExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace Hubtel.QuickHelpers
+{
+    public static class HubtelExtensions
+    {
+        public static string ClientId = "ryxqkwnm";
+        public static string Secret = "vjbipotk";
+        public static string From = "Panda"; // not in use;
+        public static string BulkSMSUrl = "https://smsc.hubtel.com/v1/messages";
+
+
+        public static async Task<string> PrepareSmsApiCall()
+        {
+            var apiUrl = BulkSMSUrl;
+            var apiClient = ClientId;
+            var apiSecret = Secret;
+
+            var data = GetCustomerDetailsFromDb();
+
+            var msg = $"Dear {data.Name}, " +
+                         $"\r\n" +
+                         $"Your reset password code is : {data.Code}. \r\n" +
+                         $"Don't share this code with others" +
+                         $"\r\n" +
+                         $"Panda";
+
+
+            var urlConst = $"{apiUrl}/send?From={data.From}&To={data.Phone}&Content={msg}&ClientID={apiClient}&ClientSecret={apiSecret}&RegisteredDelivery=true&FromToContentClientIdClientSecretRegisteredDelivery=";
+
+            HttpClient _client = new();
+
+            HttpResponseMessage resp = await _client.GetAsync(urlConst);
+
+            string response;
+            if (resp.IsSuccessStatusCode)
+            {
+                var result = await resp.Content.ReadAsStringAsync();
+
+                response = $"{result}";
+            }
+            else
+            {
+                response = $"Please try again...";
+            }
+
+            return response;
+        }
+
+        private static HubtelDto GetCustomerDetailsFromDb()
+            => new()
+            {
+                Name = "Michael",
+                Phone = "0553771219",
+                From = "0276002658",
+                Code = Guid.NewGuid().ToString(),
+            };
+    }
+}
